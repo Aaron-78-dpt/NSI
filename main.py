60eaf7067c6af883107d22651e472a65c4e49ff6
Aaron-78-dpt <110103315ch@ndoverneuil.net>
2026-01-20 14:46:35 +0100

    Add necessary imports for API and plotting
    Added imports for requests, json, matplotlib, and numpy.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import requests # permet d'utiliser des API 
+import json
+import matplotlib.pyplot as plt # permet al creation de graphique 
+import numpy as np
+
+
 class Pokemon():
     def __init__(self,nom = "",id  = ""):
         
