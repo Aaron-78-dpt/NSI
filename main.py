dbdfec676b2adc9dce529f09d5b30c4515454b64
110494302eh-code <110494302eh@ndoverneuil.net>
2026-01-20 14:59:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,32 @@ class Pokemon():
         r = requests.get(f"https://pokeapi.co/api/v2/type/{id}/")
 
 Charizard = Pokemon(35)
+
+
+
+import requests # permet d'utiliser des API 
+import json
+import matplotlib.pyplot as plt # permet al creation de graphique 
+import numpy as np
+
+class Pokemon():
+    def __init__(self,nom = "",id  = ""):
+        
+        self.nom = nom
+        self.id = id
+
+        pass
+
+    def get_id(self):
+        r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{self.id}/?limit=10")
+        r.json()
+        return r["id"]
+
+    def get_type(self):
+        r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{id}/?limit=10")
+
+Charizard = Pokemon(35)
+
+r = requests.get(f"https://pokeapi.co/api/v2/type/id")
+r = r.json()
+print(r)
