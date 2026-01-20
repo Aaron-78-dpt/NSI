531e60dc2c60ad7ad5b8eea3e99bd8decc1a9815
Aaron-78-dpt <110103315ch@ndoverneuil.net>
2026-01-20 15:56:54 +0100

    Refactor Pokemon class and add Pokedex functionality
    Refactor Pokemon class and add Pokedex class with methods to create and manage a Pokedex.

--- main.py
+++ main.py
@@ -4,41 +4,56 @@ import matplotlib.pyplot as plt # permet al creation de graphique
 import numpy as np
 
 
-class Pokemon():
-    def __init__(self,nom = "",id  = ""):
-        
-        self.nom = nom
-        self.id = id
-
-        pass
-
-    def get_type(self):
-        r = requests.get(f"https://pokeapi.co/api/v2/type/{id}/")
-
-Charizard = Pokemon(35)
-
-
-
-import requests
-
 class Pokemon:
     def __init__(self, nom="", id=None):
         self.nom = nom
         self.id = id
 
     def get_id(self):
-        r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{self.id}")
+        r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{self.nom.lower()}")
         data = r.json()
         return data["id"]
+    
+    def get_nom(self):
+        r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{self.id}")
+        data = r.json()
+        return data["name"]
 
     def get_type(self):
         r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{self.id}")
         data = r.json()
         return [t["type"]["name"] for t in data["types"]]
 
-    def graphique()
+class Pokedex():
+    def __init__(self,nom = ""):
+        
+        self.nom = nom
+        
+        pass
+    
+    def creer_pokedex(self):
+        pokedex = []
+        return pokedex
 
-Charizard = Pokemon(id=6)
+    def creative_pokedex(self):
+        r = requests.get(f"https://pokeapi.co/api/v2/pokemon/")
+        data = r.json()
+        pokedex_lst = []
+        for pok in range(1,1351):
+            pokemon = Pokemon(nom = "", id = pok)
+            pokedex_lst.append(pokemon)
+        return pokedex_lst
+    
+    def ajouter_pokemon(self,pokemon):
+        self.
 
-print(Charizard.get_id())
-print(Charizard.get_type())
+
+    pass
+
+
+cheat = Pokedex("Pokedex cheaté ! ")
+cheat = cheat.creative_pokedex()
+print(cheat[6].get_type())
+Charizard = Pokemon(nom = "Charizard")
+Charizard.get_id()
+#print(Charizard.get_type())
